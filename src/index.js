45e06786b4ccca38dbc95af9f709ebca4d959efd
Pierre <pgadea7@gmail.com>
2017-10-16 20:34:11 -0500

    final version 1 of code news website

--- src/index.js
+++ src/index.js
@@ -1,8 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import JavaScript from './components/JavaScript';
+import Csharp from './components/Csharp';
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () =>
+<Router>
+    <div>
+
+        <Navbar>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <Link to="/">CODE NEWS</Link>
+                </Navbar.Brand>
+
+                <Navbar.Toggle />
+            </Navbar.Header>
+
+            <Navbar.Collapse>
+            <Nav>
+                <NavItem>
+                  <NavLink exact to="/" activeClassName="active">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink to="/javascript" activeClassName="active">JavaScript</NavLink>
+                </NavItem>
+                  <NavItem>
+                  <NavLink to="/csharp" activeClassName="active">Csharp</NavLink>
+                </NavItem>
+            </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+
+        <Route exact path="/" component={ App } />
+        <Route exact path="/javascript" component={ JavaScript } />
+        <Route exact path="/csharp" component={ Csharp } />
+    </div>
+</Router>
+
+const About = () =>
+<div>
+  <h1>This is about page...</h1>
+</div>
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
